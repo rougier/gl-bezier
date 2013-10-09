f3e6d198ad84dcb22e17c530affeaf7ea247e3e3
Nicolas Rougier <Nicolas.Rougier@inria.fr>
2013-10-09 11:13:50 +0200

    vec2 class for point/vector of 2 floats

---
+++ vec2.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+import math
+
+class vec2(list):
+
+    def __init__ (self, x=0, y=0):
+        self.x = float (x)
+        self.y = float (y)
+
+    def __repr__ (self):
+        return "vec2(%g,%g)" % (self.x, self.y)
+
+    def __eq__ (self, other):
+        return self.x == other.x and self.y == other.y
+
+    def __add__ (self, other):
+        return vec2(self.x+other.x, self.y+other.y)
+
+    def __iadd__ (self, other):
+        self.x += other.x
+        self.y += other.y
+        return self
+
+    def __getitem__ (self, index):
+        if   index == 0: return self.x
+        elif index == 1: return self.y
+        raise IndexError
+
+    def __setitem__ (self, index, value):
+        if   index == 0: self.x = value
+        elif index == 1: self.y = value
+        raise IndexError
+
+    def __len__ (self):
+        return 2
+
+    def __iter__(self):
+        for v in (self.x, self.y):
+            yield v
+
+    def __sub__ (self, other):
+        return vec2(self.x-other.x, self.y-other.y)
+
+    def __isub__ (self, other):
+        self.x -= other.x
+        self.y -= other.y
+        return self
+
+    def __neg__ (self):
+        return vec2(-self.x, -self.y)
+
+    def __mul__ (self, value):
+        return vec2(self.x*value, self.y*value)
+
+    def __rmul__ (self, value):
+        return vec2(self.x*value, self.y*value)
+
+    def __div__ (self, value):
+        return vec2(self.x/value, self.y/value)
+
+    def __rdiv__ (self, value):
+        return vec2(self.x/value, self.y/value)
+
+    def length(self):
+        return math.hypot(self.x, self.y)
+
+    def angle(self):
+        return math.atan2 (self.y, self.x)
+
+    def ortho(self):
+        return vec2(-self.y, self.x)
+
+    def normalized(self):
+        length = math.hypot(self.x, self.y)
+        if not length:
+            return vec2(self.x, self.y)
+        return vec2(self.x/length, self.y /length)
+
+    def rebase (self, bx, by = None):
+        if by is None:
+            by = bx.ortho()
+        return vec2(self*bx,self*by)
+
+
+def normalized(v):
+    return v.normalized()
+
+def length(v):
+    return v.length()
+
+def dot(v0, v1):
+    return v0.x*v1.x + v0.y*v1.y
+
+def cross(v0, v1):
+    return v0.x*v1.y - v0.y*v1.x 
+
+def mix(v0, v1, a):
+    if a == 0.0: return vec2(v0.x, v0.y)
+    if a == 1.0: return vec2(v1.x, v1.y)
+    return vec2 ((1-a)*v0.x + a*v1.x,
+                 (1-a)*v0.y + a*v1.y)
+
+def middle(v0, v1):
+    return mix(v0,v1,0.5)
+
+def angle(v0,v1):
+    return math.atan2(abs(cross(v0,v1)),dot(v0,v1))
+
+def det(p0, p1, p2):
+    return (p1.x-p0.x)*(p2.y-p0.y) - (p1.y-p0.y)*(p2.x-p0.x)
